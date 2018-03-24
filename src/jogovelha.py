f745add03a5357239be39b5f55982392d447fe26
Kallyne <36283393+Kallyne@users.noreply.github.com>
2018-03-23 22:09:47 -0300

    Update jogovelha.py
    #12

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,27 +1,83 @@
-TAB = []
+# O tabuleiro
+velha="""               Posições
+   |   |      7 | 8 | 9
+---+---+---  ---+---+---
+   |   |      4 | 5 | 6
+---+---+---  ---+---+---
+   |   |      1 | 2 | 3
+"""
+# Uma lista de posições (linha e coluna) para cada posição válida do jogo
+# Um elemento extra foi adicionado para facilitar a manipulação
+# dos índices e para que estes tenham o mesmo valor da posição
+#
+#  7 | 8 | 9
+# ---+---+---
+#  4 | 5 | 6
+# ---+---+---
+#  1 | 2 | 3
 
-def inicializar() :
-  TAB.append(['.','.','.'])
-  TAB.append(['.','.','.'])
-  TAB.append(['.','.','.'])
+posições = [
+       None,  # Elemento adicionado para facilitar índices
+      (5, 1), # 1
+      (5, 5), # 2
+      (5, 9), # 3
+      (3, 1), # 4
+      (3, 5), # 5
+      (3, 9), # 6
+      (1, 1), # 7
+      (1, 5), # 8
+      (1, 9), # 9
+    ]
 
-def jogar(jogador, linha, coluna):
-  if jogador !='X' and jogador != 'O':
-    raise RuntimeError('Jogador inválido!')
-  valores = list(range(0,3))
-  if linha not in valores:
-    raise RuntimeError('Linha inválida!')
-  if coluna not in valores:
-    raise RuntimeError('Coluna inválida!')
-  TAB[linha][coluna] = jogador
+# Posições que levam ao ganho do jogo
+# Jogadas fazendo uma linha, um coluna ou as diagonais ganham
+# Os números representam as posições ganhadoras
+ganho = [
+          [ 1, 2, 3], # Linhas
+          [ 4, 5, 6],
+          [ 7, 8, 9],
+          [ 7, 4, 1], # Colunas
+          [ 8, 5, 2],
+          [ 9, 6, 3],
+          [ 7, 5, 3], # Diagonais
+          [ 1, 5, 9]
+        ]
 
-def tabuleiro():
-  return TAB
+# Constroi o tabuleiro a partir das strings
+# gerando uma lista de listas que pode ser modificada
+tabuleiro = []
+for linha in velha.splitlines():
+    tabuleiro.append(list(linha))
 
-def main():
-  inicializar()
-  jogar('X', 1, 1)
-  print(tabuleiro())
-
-if __name__ == "__main__":
-  main()
+jogador = "X" # Começa jogando com X
+jogando = True
+jogadas = 0 # Contador de jogadas - usado para saber se velhou
+while True:
+    for t in tabuleiro:  # Imprime o tabuleiro
+        print("".join(t))
+    if not jogando: # Termina após imprimir o último tabuleiro
+        break
+    if jogadas == 9: # Se 9 jogadas foram feitas, todas as posições já foram preenchidas
+        print("Deu velha! Ninguém ganhou.")
+        break
+    jogada = int(input("Digite a posição a jogar 1-9 (jogador %s):" % jogador))
+    if jogada<1 or jogada>9:
+        print("Posição inválida")
+        continue
+    # Verifica se a posição está livre
+    if tabuleiro[posições[jogada][0]][posições[jogada][1]] != " ":
+        print("Posição ocupada.");
+        continue
+    # Marca a jogada para o jogador
+    tabuleiro[posições[jogada][0]][posições[jogada][1]] = jogador
+    # Verfica se ganhou
+    for p in ganho:
+        for x in p:
+            if tabuleiro[posições[x][0]][posições[x][1]] != jogador:
+                break
+        else: # Se o for terminar sem break, todas as posicoes de p pertencem ao mesmo jogador
+            print("O jogador %s ganhou (%s): "%(jogador, p))
+            jogando = False
+            break
+    jogador = "X" if jogador == "O" else "O" # Alterna jogador
+    jogadas +=1 # Contador de jogadas
